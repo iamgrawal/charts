b9692689ddb3528b860fa6bf6e913981c951829b
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-03-30 16:46:01 +0530

    Graphs Added

--- main.js
+++ main.js
@@ -1,4 +1,58 @@
-var lineGraph = document.getElementById('canvas');
+var barGraph = document.getElementById('barGraph');
+var barGraphData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      fillColor: 'rgba(172,194,132,0.4)',
+      strokeColor: '#ACC26D',
+      pointColor: '#fff',
+      pointStrokeColor: '#9DB86D',
+      data: [203000, 15600, 99000, 25100, 30500, 24700]
+    }
+  ]
+};
+const barGraphObject = new Chart(barGraph, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
+
+var lineGraph = document.getElementById('lineGraph');
 var lineGraphData = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June'],
   datasets: [
@@ -12,7 +66,61 @@ var lineGraphData = {
   ]
 };
 const lineGraphObject = new Chart(lineGraph, {
-  type: 'bar',
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
+
+var pieGraph = document.getElementById('pieGraph');
+var pieGraphData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      fillColor: 'rgba(172,194,132,0.4)',
+      strokeColor: '#ACC26D',
+      pointColor: '#fff',
+      pointStrokeColor: '#9DB86D',
+      data: [203000, 15600, 99000, 25100, 30500, 24700]
+    }
+  ]
+};
+const pieGraphObject = new Chart(pieGraph, {
+  type: 'pie',
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
